b02c08d7df7af926aded5e9f36a776d8c0160361
ahmetaltuntas <ahmet34w@gmail.com>
2022-04-07 03:58:58 +0300

    Admin

--- resources/views/layouts/adminbase.blade.php
+++ resources/views/layouts/adminbase.blade.php
@@ -4,7 +4,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Corona Admin</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{asset("assets/admin")}}/vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="{{asset("assets/admin")}}/vendors/css/vendor.bundle.base.css">
@@ -21,16 +20,37 @@
     <link rel="stylesheet" href="{{asset("assets/admin")}}/css/style.css">
     <!-- End layout styles -->
     <link rel="shortcut icon" href="{{asset("assets/admin")}}/images/favicon.png" />
+    <script src="{{asset("assets/admin")}}/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="{{asset("assets/admin")}}/vendors/chart.js/Chart.min.js"></script>
+    <script src="{{asset("assets/admin")}}/vendors/progressbar.js/progressbar.min.js"></script>
+    <script src="{{asset("assets/admin")}}/vendors/jvectormap/jquery-jvectormap.min.js"></script>
+    <script src="{{asset("assets/admin")}}/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="{{asset("assets/admin")}}/vendors/owl-carousel-2/owl.carousel.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{asset("assets/admin")}}/js/off-canvas.js"></script>
+    <script src="{{asset("assets/admin")}}/js/hoverable-collapse.js"></script>
+    <script src="{{asset("assets/admin")}}/js/misc.js"></script>
+    <script src="{{asset("assets/admin")}}/js/settings.js"></script>
+    <script src="{{asset("assets/admin")}}/js/todolist.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <script src="{{asset("assets/admin")}}/js/dashboard.js"></script>
     <title>@yield('title')</title>
     @yield('head')
 </head>
-@include('admin.header')
+<body>
+@include('admin.sidebar')
+@include('admin.navbar')
+
 
 
 
 @yield('content')
 
-@include('home.footer')
+@include('admin.footer')
 @yield('foot')
 </body>
 </html>
